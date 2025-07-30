2df393f322b92b280a12d2771e72e2af7868c6ea
CoronelDalma <coronel.dg@gmail.com>
2025-07-30 15:27:47 -0300

    FIX: cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const tasksRouter = require('../src/routes/task.routes');
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 // Path for API routes
