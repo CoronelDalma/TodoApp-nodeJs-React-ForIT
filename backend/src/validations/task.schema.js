867c96c0d2aebb474dae279b93e80cd98762d4eb
CoronelDalma <coronel.dg@gmail.com>
2025-07-30 17:53:26 -0300

    Fix: validations

--- backend/src/validations/task.schema.js
+++ backend/src/validations/task.schema.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const taskSchema = Joi.object({
     title: Joi.string().min(3).max(100).required(),
     description: Joi.string().max(500).optional(),
-    completed: Joi.boolean().default(false)
+    completed: Joi.boolean().default(false),
+    createdAt: Joi.date().optional()
 });
 
 module.exports = taskSchema;
